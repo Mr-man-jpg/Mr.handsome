21794c3d7d4b150a601f3a670884a0b711faf386
Mr-man-jpg <3368860548@qq.com>
2025-11-01 19:15:11 +0800

    Create 5_main.c

---
+++ 5_main.c
@@ -0,0 +1,31 @@
+//202510116309 
+//3368860548@qq.com
+//吴振华
+#include <stdio.h>
+
+int main() 
+{
+    int arr[5]; 
+    
+    for (int i = 0; i < 4; i++) 
+    {
+        scanf("%d", &arr[i]);
+    }
+    
+    arr[4] = 0; 
+    for (int i = 0; i < 4; i++) 
+    {
+        arr[4] += arr[i];
+    }
+    
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d", arr[i]);
+        if (i < 4) 
+        {
+            printf(" "); 
+        }  
+    }
+    
+    return 0;
+}
