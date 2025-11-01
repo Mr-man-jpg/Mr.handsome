f25a331726e417be2ef4c2a67707bd55bbeb41c4
Mr-man-jpg <3368860548@qq.com>
2025-11-01 19:09:10 +0800

    Update 3_main.c
    Refactored main function to include Armstrong number calculation.

--- 3_main.c
+++ 3_main.c
@@ -2,37 +2,27 @@
 //3368860548@qq.com
 //吴振华
 #include <stdio.h>
-
-int main() {
-    int a, b;
-    char op;
+int main() 
+{
+    int count = 0; 
     
-    // 输入两个整数和运算符
-    printf("请输入两个整数和一个运算符（+、-、*、/）：");
-    scanf("%d %d %c", &a, &b, &op);
-    
-    // 根据运算符进行计算
-    switch(op) {
-        case '+':
-            printf("%d + %d = %d\n", a, b, a + b);
-            break;
-        case '-':
-            printf("%d - %d = %d\n", a, b, a - b);
-            break;
-        case '*':
-            printf("%d * %d = %d\n", a, b, a * b);
-            break;
-        case '/':
-            // 检查除数是否为0
-            if(b != 0) {
-                printf("%d / %d = %.2f\n", a, b, (double)a / b);
-            } else {
-                printf("错误：除数不能为0！\n");
+    for (int num = 100; num <= 999; num++) 
+    {
+        int a = num / 100;        
+        int b = (num / 10) % 10;  
+        int c = num % 10;         
+        
+        int sum = a * a * a + b * b * b + c * c * c;
+        
+        if (sum == num) 
+        {
+            if (count > 0) 
+            {
+                printf(" "); 
             }
-            break;
-        default:
-            printf("错误：无效的运算符！\n");
-            break;
+            printf("%d", num);
+            count++;
+        }
     }
     
     return 0;
