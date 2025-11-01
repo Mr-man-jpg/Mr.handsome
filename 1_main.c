0ba6cd585831ff0836363e09b6da85f949dbae6b
Mr-man-jpg <3368860548@qq.com>
2025-11-01 19:04:53 +0800

    Update 1_main.c
    Modified the program to read an array of integers and calculate their sum.

--- 1_main.c
+++ 1_main.c
@@ -2,19 +2,30 @@
 //3368860548@qq.com
 //吴振华
 #include <stdio.h>
-
-int main() {
-    float celsius, fahrenheit;
+int main() 
+{
+    int arr[5]; 
     
-    // 获取用户输入的摄氏温度
-    printf("请输入摄氏温度：");
-    scanf("%f", &celsius);
+    for (int i = 0; i < 4; i++) 
+    {
+        scanf("%d", &arr[i]);
+    }
     
-    // 转换为华氏温度
-    fahrenheit = celsius * 9.0f / 5.0f + 32.0f;
+    arr[4] = 0; 
+    for (int i = 0; i < 4; i++) 
+    {
+        arr[4] += arr[i];
+    }
     
-    // 输出结果，保留1位小数
-    printf("华氏温度为：%.1f\n", fahrenheit);
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d", arr[i]);
+        if (i < 4) 
+        {
+            printf(" "); 
+        }  
+    }
     
     return 0;
 }
+
