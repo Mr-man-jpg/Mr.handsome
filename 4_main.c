f23765af568ba82246866ac76a40db554c0ee250
Mr-man-jpg <3368860548@qq.com>
2025-11-08 19:01:18 +0800

    Refactor main function and add arithmeticSum function

--- 4_main.c
+++ 4_main.c
@@ -2,31 +2,26 @@
 //3368860548@qq.com
 //吴振华
 #include <stdio.h>
+
+int arithmeticSum(int a1, int an, int step) 
+{
+    int n;  
+    
+    if (step == 0) 
+    {
+        return 0;  
+    }
+    
+    n = (an - a1) / step + 1;
+    
+    return n * (a1 + an) / 2;
+}
+
 int main() 
 {
-    int count = 0; 
-    int num = 100;
-
-    while ( num <= 999) 
-    {
-        int a = num / 100;        
-        int b = (num / 10) % 10;  
-        int c = num % 10;         
-        
-        int sum = a * a * a + b * b * b + c * c * c;
-        
-        if (sum == num) 
-        {
-            if (count > 0) 
-            {
-                printf(" "); 
-            }
-            printf("%d", num);
-            count++;
-        }
-        num++;
-    }
-
+  
+    int result = arithmeticSum(1, 100, 1);
+    printf("%d\n", result);
     
     return 0;
 }
