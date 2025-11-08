0a3d7832894283e0198438eb2c6851c88b7f0e53
Mr-man-jpg <3368860548@qq.com>
2025-11-08 19:13:14 +0800

    Update 2_main.c

--- 2_main.c
+++ 2_main.c
@@ -34,28 +34,4 @@ int main() {
 
 
 
-void bubbleSort(int arr[], int n) 
-{
-    int i, j, temp;
-    int swapped;
-    
-    for (i = 0; i < n - 1; i++)
-        {
-        swapped = 0;
-        for (j = 0; j < n - 1 - i; j++) 
-        {
-            if (arr[j] > arr[j + 1]) 
-            {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-                swapped = 1;
-            }
-        }
 
-        if (!swapped) 
-        {
-            break;
-        }
-    }
-}
