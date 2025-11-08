9ee6de633941fdc3a99315456090135838c78fbc
Mr-man-jpg <3368860548@qq.com>
2025-11-08 18:59:58 +0800

    Refactor power function and main logic
    Refactor power function to calculate powers and sum of cubes for three-digit numbers.

--- 3_main.c
+++ 3_main.c
@@ -2,28 +2,52 @@
 //3368860548@qq.com
 //吴振华
 #include <stdio.h>
+
+double power(int a, int b) 
+{
+    double result = 1.0;
+    int i;
+    
+
+    if (b == 0) 
+    {
+        return 1.0;
+    }
+    
+  
+    if (b > 0) 
+    {
+        for (i = 0; i < b; i++) 
+        {
+            result *= a;
+        }
+    } 
+  ）
+    else {
+        for (i = 0; i < -b; i++) 
+        {
+            result *= a;
+        }
+        result = 1.0 / result;
+    }
+    
+    return result;
+}
+
 int main() 
 {
-    int count = 0; 
+    int i;
+    double sum = 0;
     
-    for (int num = 100; num <= 999; num++) 
+
+    for (i = 1; i <= 5; i++) 
     {
-        int a = num / 100;        
-        int b = (num / 10) % 10;  
-        int c = num % 10;         
-        
-        int sum = a * a * a + b * b * b + c * c * c;
-        
-        if (sum == num) 
-        {
-            if (count > 0) 
-            {
-                printf(" "); 
-            }
-            printf("%d", num);
-            count++;
-        }
+        sum += power(i, 2);  
     }
+    
+
+    printf("%.0f\n", sum);  
     
     return 0;
 }
+
